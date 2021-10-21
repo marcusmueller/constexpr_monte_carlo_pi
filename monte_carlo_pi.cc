1d3141271985e39d74b35554138f7c9a55aaeb64
Marcus Müller <marcus@hostalia.de>
2021-10-21 21:23:03 +0200

    Made dual-header library
    Signed-off-by: Marcus Müller <marcus@hostalia.de>

--- monte_carlo_pi.cc
+++ monte_carlo_pi.cc
@@ -1,29 +1,6 @@
 // Copyright 2021 Marcus Müller
-// SPDX-License-Identifier: LGPL-3.0-or-later
-#include "xoroshiro128p.hpp"
+// SPDX-License-Identifier: GPL-3.0-or-later
+#include "monte_carlo_pi.hpp"
 #include <fmt/format.h>
-#include <cstdint>
-
-namespace monte_carlo_pi {
-constexpr rng::xoroshiro128p::result_type seed = 0xdeadbeef;
-template <typename fp_t = double>
-constexpr fp_t the_count(unsigned int howmany)
-{
-    unsigned int sum = 0;
-    rng::xoroshiro128p rng(seed);
-    for (auto idx = howmany; idx; --idx) {
-        auto result = rng();
-        uint64_t lower = result & 0xFFFFFFF;
-        uint64_t upper = (result >> 31) & 0xFFFFFFF;
-
-        std::uint64_t mag_sq = upper * upper + lower * lower;
-        if (mag_sq <= (0x1ULL << 56)) {
-            sum++;
-        }
-    }
-    return static_cast<fp_t>(4 * sum) / howmany;
-}
-constexpr auto pi = monte_carlo_pi::the_count(242500);
-} // namespace monte_carlo_pi
 
 int main() { fmt::print("{:20f}", monte_carlo_pi::pi); }
---
+++ monte_carlo_pi.hpp
@@ -0,0 +1,27 @@
+// Copyright 2021 Marcus Müller
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#pragma once
+#include "xoroshiro128p.hpp"
+#include <cstdint>
+
+namespace monte_carlo_pi {
+constexpr rng::xoroshiro128p::result_type seed = 0xdeadbeef;
+template <typename fp_t = double>
+constexpr fp_t the_count(unsigned int howmany)
+{
+    unsigned int sum = 0;
+    rng::xoroshiro128p rng(seed);
+    for (auto idx = howmany; idx; --idx) {
+        auto result = rng();
+        uint64_t lower = result & 0xFFFFFFF;
+        uint64_t upper = (result >> 31) & 0xFFFFFFF;
+
+        std::uint64_t mag_sq = upper * upper + lower * lower;
+        if (mag_sq <= (0x1ULL << 56)) {
+            sum++;
+        }
+    }
+    return static_cast<fp_t>(4 * sum) / howmany;
+}
+  //constexpr auto pi = monte_carlo_pi::the_count(242500);
+} // namespace monte_carlo_pi
